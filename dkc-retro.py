9b420cc089b00db164035fd29d88b470d22f8dca
talhaahussain <talhaahussain@gmail.com>
2023-07-09 21:11:58 +0100

    Initial prototype in dkc-retro.py
    Added game loop, random input and rendering
    Tested, Diddy Kong jumps around randomly
    No known bugs.

--- dkc-retro.py
+++ dkc-retro.py
@@ -3,3 +3,10 @@ import retro
 env = retro.make('DonkeyKongCountry-Snes', '1Player.CongoJungle.RopeyRampage.Level2')
 
 env.reset()
+
+done = False
+
+while not done:
+    env.render()
+    action = env.action_space.sample()    
+    env.step(action)
